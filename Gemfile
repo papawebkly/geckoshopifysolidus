d753e46c438f24d770294f9c004bc7e62be24173
superstar <grandpa0064@gmail.com>
2018-04-16 16:09:57 -0400

    changed EStore root_path

--- Gemfile
+++ Gemfile
@@ -40,7 +40,6 @@ gem 'font-awesome-rails'
 gem 'solidus'
 gem 'solidus_auth_devise'
 
-
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'static_pages/about'
+
+  get 'static_pages/catalog'
+
+  get 'static_pages/blog'
+
+  get 'static_pages/contact'
+
   # This line mounts Solidus's routes at the root of your application.
   # This means, any requests to URLs such as /products, will go to Spree::ProductsController.
   # If you would like to change where this engine is mounted, simply change the :at option to something different.
